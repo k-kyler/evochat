c1ecc6a59f7db82a7526491936c43f902774ba63
K-Kyler <khaiquang690@gmail.com>
2021-06-25 17:59:31 +0700

    Remove docker config

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,7 +1,43 @@
 import { FC } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 
 const Chat: FC = () => {
-  return <div>Chat</div>;
+  return (
+    <ChatContainer>
+      <ListRoomsContainer>List rooms</ListRoomsContainer>
+
+      <RoomOptionsContainer>Room options</RoomOptionsContainer>
+
+      <ChatAreaContainer>Chat area</ChatAreaContainer>
+    </ChatContainer>
+  );
 };
 
 export default Chat;
+
+const ChatContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex-col
+  `}
+`;
+
+const ListRoomsContainer = styled.div`
+  ${tw`
+  text-white
+  `}
+`;
+
+const RoomOptionsContainer = styled.div`
+  ${tw`
+    text-white
+  `}
+`;
+
+const ChatAreaContainer = styled.div`
+  ${tw`
+    text-white
+  `}
+`;
