2b94ade0dfbf536ba213447d1dd55003049f772b
K-Kyler <khaiquang690@gmail.com>
2021-07-02 22:57:41 +0700

    Add option group, options list, member item and re-make types

--- src/components/OptionsList/index.tsx
+++ src/components/OptionsList/index.tsx
@@ -1,14 +1,19 @@
-import { FC, useState, useEffect } from "react";
+import { FC } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-import { FaAngleDown } from "react-icons/fa";
-import { useRooms } from "../../contexts/RoomsContext";
+import { RoomType } from "../../typings/RoomType";
 import OptionGroup from "../OptionGroup";
 
-const OptionsList: FC = () => {
+interface IOptionsListProps {
+  selectedRoom?: RoomType;
+}
+
+const OptionsList: FC<IOptionsListProps> = ({ selectedRoom }) => {
   return (
     <OptionsListContainer>
-      <OptionGroup />
+      <OptionGroup type="members" name="Room members" />
+      <OptionGroup type="media" name="Shared media" />
+      <OptionGroup type="files" name="Shared files" />
     </OptionsListContainer>
   );
 };
@@ -18,7 +23,28 @@ export default OptionsList;
 const OptionsListContainer = styled.div`
   ${tw`
     flex-1
-    px-3
-    py-4
+    py-2
+    overflow-y-auto
   `}
+
+  /* Chrome, Edge, and Safari */
+  &::-webkit-scrollbar {
+    width: 0.47rem;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #2f3136;
+    border-top: 1px solid rgba(4, 4, 5, 0.2);
+  }
+
+  &::-webkit-scrollbar-thumb {
+    ${tw`
+      bg-gray-500
+      rounded-lg
+    `}
+  }
+
+  /* Firefox */
+  scrollbar-width: auto;
+  scrollbar-color: #6b7280 #2f3136;
 `;
--- src/typings/QueryType.ts
+++
@@ -1,3 +0,0 @@
-export type QueryType = {
-  id: string | string[] | null;
-};
